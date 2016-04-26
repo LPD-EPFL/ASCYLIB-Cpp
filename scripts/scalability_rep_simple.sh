8d4d4697d632b89a332ec9d67d25cbbfea252a11
Daniel Gray <daniel.grayvargas@epfl.ch>
2016-04-26 18:26:20 +0200

    Fix typo in loop for single core execution

--- scripts/scalability_rep_simple.sh
+++ scripts/scalability_rep_simple.sh
@@ -59,7 +59,7 @@ print_rep "#cores  " $num_algos "throughput   " "\n"
 
 printf "%-8d" 1;
 thr1="";
-for algo in $algo;
+for algo in $algos;
 do
     thr=$($run_script ./bin/test -a $algo $params -n1);
     thr1="$thr1 $thr";
